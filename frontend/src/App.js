5659bb779672472501ca7e312aa78fe8a22212fa
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-27 10:00:05 -0300

    v4.10

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import GlobalStyle from "./styles/global";
+import styled from "styled-components";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
 
 function App() {
   return (
     <>
+      <Container>
+
+      </Container>
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />;
       <GlobalStyle />;
     </>
