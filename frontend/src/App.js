7378005bb33f88c4f73b45be9efd413036b239f0
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-27 09:56:41 -0300

    v4.9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import GlobalStyle from "./styles/global";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
-      <GlobalStyle/>;
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />;
+      <GlobalStyle />;
     </>
   );
-}
+};
 
 export default App;
